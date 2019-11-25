42826aa3da16e846ca30c9683d388f25a7891c00
olarmarian <48599258+olarmarian@users.noreply.github.com>
2019-11-25 22:54:48 +0200

    User Page (Profile component)
    + modifications at movie card, tv shows component and carousel (sizes)

---
+++ src/app/src/app/app.module.ts
@@ -0,0 +1,70 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import {
+  MatToolbarModule,
+  MatSidenavModule,
+  MatListModule,
+  MatButtonModule,
+  MatIconModule,
+  MatGridListModule
+} from "@angular/material";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { MatInputModule } from "@angular/material";
+import { MatMenuModule } from "@angular/material/menu";
+import { HomeComponent } from "./home/home.component";
+import { CalendarComponent } from "./calendar/calendar.component";
+import { LoginComponent } from "./components/login/login.component";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatCardModule } from "@angular/material/card";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MovieCardComponent } from "./components/movie-card/movie-card.component";
+import { SlickCarouselModule } from "ngx-slick-carousel";
+import { CarouselComponent } from "./components/carousel/carousel.component";
+import { SlideshowModule } from "ng-simple-slideshow";
+import { TvShowsComponent } from "./tv-shows/tv-shows.component";
+import { ProfileComponent } from './profile/profile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CalendarComponent,
+    LoginComponent,
+    MovieCardComponent,
+    CarouselComponent,
+    TvShowsComponent,
+    ProfileComponent
+  ],
+  imports: [
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatCardModule,
+    BrowserModule,
+    AppRoutingModule,
+    MatGridListModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatMenuModule,
+    MatCardModule,
+    SlickCarouselModule,
+    SlideshowModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/src/app/tv-shows/tv-shows.component.ts
@@ -0,0 +1,97 @@
+import { TV_SERIES } from "./../apis/binge-watch.mock";
+import { ActivatedRoute } from "@angular/router";
+import { Component, OnInit } from "@angular/core";
+import { TvSeries } from "../models/tvSeries";
+import { Movie } from "../models/movie";
+import * as mockData from "../apis/binge-watch.mock";
+import { MOVIE_LIST } from "ProiectColectiv-Frontend/src/app/apis/binge-watch.mock";
+
+@Component({
+  selector: "app-tv-shows",
+  templateUrl: "./tv-shows.component.html",
+  styleUrls: ["./tv-shows.component.scss"]
+})
+export class TvShowsComponent implements OnInit {
+  public data: any[];
+  public images: string[] = [];
+  public breakpoint: number = 4;
+  public ratio: string;
+  public type: string;
+  constructor(private route: ActivatedRoute) {
+    this.imagesUrl();
+  }
+
+  public imagesUrl(): string[] {
+    mockData.MOVIE_LIST.forEach(x => {
+      if (this.images.filter(y => y === x.image).length === 0)
+        this.images.push(x.image);
+    });
+
+    return this.images;
+  }
+
+  ngOnInit() {
+    this.route.data.subscribe(data => {
+      this.type = data.type;
+    });
+
+    switch (this.type) {
+      case "movies":
+        this.data = mockData.MOVIE_LIST;
+        break;
+      case "tvshows":
+        this.data = mockData.TV_SERIES;
+        break;
+      default:
+        alert("Ceva fain");
+    }
+
+    /* fetch data from server
+    switch (this.type) {
+      case "movie":
+        fetch("url")
+          .then(response => response.json())
+          .then(json => {
+            json.forEach(element => {
+              const movie = {
+                id: element.id,
+                name: element.name,
+                duration: element.duration,
+                releaseDate: element.releaseDate,
+                author: element.author,
+                description: element.description,
+                image: element.image
+              };
+              this.data.push(movie);
+            });
+          })
+          .catch(err => {
+            alert(err);
+          });
+        break;
+      case "tvshows":
+        fetch("url")
+          .then(response => response.json())
+          .then(json => {
+            json.forEach(element => {
+              const serial = {
+                image: element.image,
+                id: element.id,
+                name: element.name,
+                releaseDate: element.releaseDate,
+                noEpisodes: element.noEpisodes,
+                noSeasons: element.noSeasons
+              };
+              this.data.push(serial);
+            });
+          })
+          .catch(err => {
+            alert(err);
+          });
+        break;
+      default:
+        alert("Try to refresh");
+    }
+    */
+  }
+}
