bf2ebdc98e6067dda33084c87266b805d18a8f7e
olarmarian <olar.marian7@gmail.com>
2020-01-12 21:31:12 +0200

    update movie / series interfaces

--- src/app/models/movie.ts
+++ src/app/models/movie.ts
@@ -1,17 +1,19 @@
 export interface MovieInterface {
-    name: string,
-    duration: number,
-    releaseDate: Date,
-    author: string,
-    description: string,
-    image: string    
+  name: string;
+  duration: number;
+  releaseYear: string;
+  director: string;
+  description: string;
+  image: string;
+  genres: string;
 }
 
 export class Movie implements MovieInterface {
-    name: string;    
-    duration: number;
-    releaseDate: Date;
-    author: string;
-    description: string;
-    image: string;
+  name: string;
+  duration: number;
+  releaseYear: string;
+  director: string;
+  genres: string;
+  description: string;
+  image: string;
 }
