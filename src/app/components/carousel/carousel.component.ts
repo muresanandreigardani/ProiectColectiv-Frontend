d6c6955f588eb20f0c15dfc4c6ca383536d05c2d
cezara98t <52743557+cezara98t@users.noreply.github.com>
2020-01-18 12:58:46 +0200

    solved carousel bug

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -8,13 +8,21 @@ import { Movie } from "src/app/models/movie";
 })
 export class CarouselComponent {
   public innerWidth: number;
-  public nrOfMovies: number = 8;
+  public nrOfMovies: number;
+  public toShow: number;
   @Input()
   public title: string;
   @Input()
   public movieList: any[];
+
+  ngOnInit() {
+    this.nrOfMovies = this.movieList.length;
+    if (this.nrOfMovies > 8) this.slideConfig.slidesToShow = 8;
+    else this.slideConfig.slidesToShow = 1;
+  }
+
   public slideConfig = {
-    slidesToShow: 8,
+    slidesToShow: 1,
     slidesToScroll: 2,
     infinite: false
   };
