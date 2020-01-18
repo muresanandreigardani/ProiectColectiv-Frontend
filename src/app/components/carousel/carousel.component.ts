5a03e4167989535fcc16a6e1ba57abdeb872be2e
cezara98t <52743557+cezara98t@users.noreply.github.com>
2020-01-18 11:58:54 +0200

    solved caroousel

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -13,19 +13,9 @@ export class CarouselComponent {
   public title: string;
   @Input()
   public movieList: any[];
-
   public slideConfig = {
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    infinite: true,
-    // responsive: [
-    //   { breakpoint: 1340, settings: { slidesToShow: 7, slidesToScroll: 7 } },
-    //   { breakpoint: 1260, settings: { slidesToShow: 6, slidesToScroll: 6 } },
-    //   { breakpoint: 1080, settings: { slidesToShow: 5, slidesToScroll: 5 } },
-    //   { breakpoint: 900, settings: { slidesToShow: 4, slidesToScroll: 4 } },
-    //   { breakpoint: 720, settings: { slidesToShow: 3, slidesToScroll: 3 } },
-    //   { breakpoint: 540, settings: { slidesToShow: 2, slidesToScroll: 2 } },
-    //   { breakpoint: 360, settings: { slidesToShow: 1, slidesToScroll: 1 } }
-    // ]
+    slidesToShow: 8,
+    slidesToScroll: 2,
+    infinite: false
   };
 }
